b46aa91450237b8c2b67a15508b29d1441e07557
Michael <michael.elliston95@gmail.com>
2025-10-10 13:15:19 -0500

    added home screen that loops unless Exit is selected

--- src/main/java/AccountingLedger.java
+++ src/main/java/AccountingLedger.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class AccountingLedger {
+
+    public static void main(String[] args) {
+
+        Scanner myScanner = new Scanner(System.in);
+        String menuSelection;
+
+        do {
+
+            System.out.print("------------- Home Screen -------------\n\tD) Add Deposit\n\tP) Make Payment (Debit)\n\tL) Ledger\n\tX) Exit\nPlease input the character that corresponds to your selection: ");
+            menuSelection = myScanner.nextLine();
+
+        } while (!Objects.equals(menuSelection, "X"));
+    }
 }
