eb4b3cec2f670c9d964d2008ccfa63aae0a398df
Michael <michael.elliston95@gmail.com>
2025-10-10 14:00:24 -0500

    Made home screen display an error properly, and loops unless X is the value of menuSelection

--- src/main/java/AccountingLedger.java
+++ src/main/java/AccountingLedger.java
@@ -10,9 +10,27 @@ public class AccountingLedger {
 
         do {
 
-            System.out.print("------------- Home Screen -------------\n\tD) Add Deposit\n\tP) Make Payment (Debit)\n\tL) Ledger\n\tX) Exit\nPlease input the character that corresponds to your selection: ");
+            System.out.print("""
+                    
+                    ------------- Home Screen -------------
+                    \t\
+                    D) Add Deposit
+                    \tP) Make Payment (Debit)
+                    \tL) Ledger
+                    \tX) Exit\
+                    
+                    Please input the character that corresponds to your selection:\s
+                    """);
             menuSelection = myScanner.nextLine();
 
+            switch (menuSelection) {
+                case "D" -> System.out.println("Adding deposit!");
+//                case "P" -> makePayment;
+//                case "L" -> displayLedger;
+                case "X" -> System.out.println("Thanks for using my application!");
+
+                default -> System.err.println("Invalid input! Please input a valid character.");
+            }
         } while (!Objects.equals(menuSelection, "X"));
     }
 }
